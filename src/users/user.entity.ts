e5b087e8d31b34239f35c0d9bb737510b84836c8
arallain <75613930+Pataubeur@users.noreply.github.com>
2022-10-21 10:21:08 +0200

    Initial commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,11 @@
+export class User {
+    id: number;
+    lastname: string;
+    firstname: string;
+
+    constructor(id: number, lastname: string, firstname: string) {
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+    }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,63 @@
+import { Controller, Get, Body, Post, Param, Put, Delete, HttpException, HttpStatus } from '@nestjs/common';
+
+import{ User } from './user.entity';
+
+const users : User[] = [
+    {
+    id:0,
+    lastname:'Doe',
+    firstname:'John'
+    }
+]
+
+@Controller('users')
+export class UsersController {
+
+@Get('all')
+getAll(): string[] {
+    return ['oui', 'c\'est', 'moi'];
+}
+
+@Get()
+getUsers(): User[] {
+    return users;
+}
+
+@Get(':id')
+getById(@Param() parameter): User {
+    let userById = users.filter(user => +user.id === +parameter.id);
+    if (userById.length === 0) {
+        throw new HttpException(`Pas d'utilisateur avec pour id ${parameter.id}`, HttpStatus.NOT_FOUND)
+    }
+    return userById[0];
+}
+
+@Post()
+create(@Body() input: any): User {
+    let id = users.length;
+    let user1 = new User(id, input.lastname, input.firstname);
+    users.push(user1);
+    return user1;
+}
+
+@Put(':id')
+put(@Param() parameter, @Body() input) : User {
+    if(input.lastname !== undefined) {
+        this.getById(parameter).lastname = input.lastname;
+    }
+    if(input.firstname !== undefined) {
+        this.getById(parameter).firstname = input.firstname;
+    }
+    return this.getById(parameter);
+}
+
+@Delete(':id')
+deleteById(@Param() parameter) : boolean {
+    let userToDelete = this.getById(parameter);
+    let index = users.findIndex(user => user.id === userToDelete.id);
+    let deletedUser = users.splice(index, 1);
+    return (deletedUser.length !== 0);
+}
+
+
+}
