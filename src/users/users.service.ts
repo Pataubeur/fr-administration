ba6e5c4b4324da9d13dfeb7814179e3c1aa1b272
arallain <75613930+Pataubeur@users.noreply.github.com>
2022-10-25 11:52:12 +0200

    Classe association mise en place avec tests validés

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,47 +14,47 @@ const users : User[] = [
 @Injectable()
 export class UsersService {
 
-getAll(): string[] {
-    return ['oui', 'c\'est', 'moi'];
-}
-
-getUsers(): User[] {
-    return users;
-}
-
-getById(id: number): User {
-    let userById = users.filter(user => +user.id === +id);
-    if (userById.length === 0) {
-        return undefined;
+    getAll(): string[] {
+        return ['oui', 'c\'est', 'moi'];
     }
-    return userById[0];
-}
 
-create(lastname: string, firstname: string, age: number) {
-    let id = users.length;
-    let userToCreate = new User(id, lastname, firstname, age);
-    users.push(userToCreate);
-    return userToCreate;
-}
+    getUsers(): User[] {
+        return users;
+    }
 
-put(id: number, lastname: string, firstname: string, age: number) : User {
-    if(lastname !== undefined) {
-        this.getById(id).lastname = lastname;
+    getById(id: number): User {
+        let userById = users.filter(user => +user.id === +id);
+        if (userById.length === 0) {
+            return undefined;
+        }
+        return userById[0];
     }
-    if(firstname !== undefined) {
-        this.getById(id).firstname = firstname;
-    }
-    if(age !== undefined) {
-        this.getById(id).age = age;
-    }
-    return this.getById(id);
-}
 
-deleteById(id: number) : boolean {
-    let userToDelete = this.getById(id);
-    let index = users.findIndex(user => user.id === userToDelete.id);
-    let deletedUser = users.splice(index, 1);
-    return (deletedUser.length !== 0);
-}
+    create(lastname: string, firstname: string, age: number) {
+        let id = users.length;
+        let userToCreate = new User(id, lastname, firstname, age);
+        users.push(userToCreate);
+        return userToCreate;
+    }
+
+    put(id: number, lastname: string, firstname: string, age: number) : User {
+        if(lastname !== undefined) {
+            this.getById(id).lastname = lastname;
+        }
+        if(firstname !== undefined) {
+            this.getById(id).firstname = firstname;
+        }
+        if(age !== undefined) {
+            this.getById(id).age = age;
+        }
+        return this.getById(id);
+    }
+
+    deleteById(id: number) : boolean {
+        let userToDelete = this.getById(id);
+        let index = users.findIndex(user => user.id === userToDelete.id);
+        let deletedUser = users.splice(index, 1);
+        return (deletedUser.length !== 0);
+    }
 
 }
