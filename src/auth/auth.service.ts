bda9b174e14e44bef9904cb06298bad70e14b6ca
FenrirWolf4566 <bdezordo@gmail.com>
2022-11-22 08:26:54 +0100

    Auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    public async validateUser(id: number, password: string) : Promise<User> {
+        /** To be implemented **/
+    }
+
+}
