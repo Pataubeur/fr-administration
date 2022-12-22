868ea8035da384f9844193fbe3b81babc36fe0eb
Pataubeur <wild2me35@gmail.com>
2022-12-22 15:34:56 +0100

    Multiorigine

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors(
+         { 'origin':'http://localhost:4200' }
+       );
+
   app.use(helmet());
 
   const config = new DocumentBuilder()
